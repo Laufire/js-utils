690eccf9256e8e3e79386ea8559dea6250d105fa
laufire <laufiretechnologies@gmail.com>
2019-05-17 12:08:52 +0530

    Added collection.clone.

--- test/collection.test.js
+++ test/collection.test.js
@@ -1,6 +1,6 @@
 /* Tested */
 const {
-	clean, collect, entries,
+	clean, clone, collect, entries,
 	flip, flipMany, fromEntries,
 	props, result, select, traverse } = require('../src/collection');
 
@@ -13,18 +13,33 @@ describe('Collections', () => {
 	};
 
 	const nestedObj = {
+		a: 1, b: 2,
+		c: {
+			d: 1,
+		},
+	};
+
+	const complexObject = {
 		single: 'single',
 		parent: {
-			child: 'child',
+			child: {
+				grandChild: 'grandChild',
+			},
 			'escaped/child': 'escaped/child',
 			'un\\/escaped\\/child': 'un\\/escaped\\/child',
 		},
 		undeifinedProp: undefined,
+		array: [1, 2],
+		complexArray: [
+			{
+				innerArray: [1, 3],
+			},
+		],
 	};
 
 	/* Tests */
 	test('clean should remove undefined props', () => {
-		expect(clean(nestedObj)).not.toHaveProperty('undefinedProperty');
+		expect(clean(complexObject)).not.toHaveProperty('undefinedProperty');
 	});
 
 	test('collect should work with all the properties of the object and build a new object', () => {
@@ -37,22 +52,26 @@ describe('Collections', () => {
 	});
 
 	test('traverse should recursively traverse through a given object and build a new object', () => {
-		const simpleObj = {
-			a: 1, b: 2,
-			c: {
-				d: 1,
-			},
-		};
-
 		const cb = (val, key) => key + val;
 
-		expect(traverse(simpleObj, cb)).toEqual({
+		expect(traverse(nestedObj, cb)).toEqual({
 			a: 'a1',
 			b: 'b2',
 			c: {
 				d: 'd1',
 			},
 		});
+	});
+
+	test('clone should recursively clone given object', () => {
+		cloned = clone(complexObject);
+
+		// Verify equality.
+		expect(cloned).toEqual(complexObject);
+
+		// Verify immutability.
+		cloned.complexArray[0].innerArray[0] = Symbol();
+		expect(cloned).not.toEqual(complexObject);
 	});
 
 	test('flip should swap the keys and values of the given object', () => {
@@ -87,9 +106,12 @@ describe('Collections', () => {
 	});
 
 	test('result should work for normal paths esacped paths', () => {
-		expect(result(nestedObj, 'single')).toEqual('single');
-		expect(result(nestedObj, 'parent/child')).toEqual('child');
-		expect(result(nestedObj, 'parent/escaped\\/child')).toEqual('escaped/child');
-		expect(result(nestedObj, 'parent/un\\\\\\/escaped\\\\\\/child')).toEqual('un\\/escaped\\/child');
+		expect(result(complexObject, 'single')).toEqual(complexObject.single);
+		expect(result(complexObject, 'parent/child')).toEqual(complexObject.parent.child);
+		expect(result(complexObject, 'parent/escaped\\/child'))
+			.toEqual(complexObject.parent['escaped/child']);
+		expect(result(complexObject, 'parent/un\\\\\\/escaped\\\\\\/child'))
+			.toEqual(complexObject.parent['un\\/escaped\\/child']);
+		expect(result(complexObject, 'non-existent')).toEqual(undefined);
 	});
 });
