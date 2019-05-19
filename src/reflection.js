6f9f500095e3be46361800fd5a8fe4a66c4345e6
laufire <laufiretechnologies@gmail.com>
2019-05-19 15:26:50 +0530

    Introduced the module, reflection.

--- src/reflection.js
+++ src/reflection.js
@@ -5,6 +5,24 @@
 const getConstructorName = (value) =>
 	value !== null && value !== undefined && value.constructor && value.constructor.name;
 
+const isObject = (value) =>
+	getConstructorName(value) == 'Object';
+
+const isFunction = (value) => //NOTE: Unlike inferType, this function doesn't differentiate between functions and async functions.
+	typeof value == 'function';
+
+const inferType = (value) => {
+	const type = typeof value;
+
+	return type !== 'object' ? type
+		: value !== null
+			? value.constructor.name.toLowerCase()
+			: 'null';
+}
+
 module.exports = {
 	getConstructorName,
+	isObject,
+	isFunction,
+	inferType,
 }
