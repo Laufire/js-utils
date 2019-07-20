e0a36651b7617531fbc0e140b90f9d0030accbd2
laufire technologies <laufiretechnologies@gmail.com>
2019-07-20 18:59:52 +0530

    Rewrote the test descriptions.

--- test/reflection.test.js
+++ test/reflection.test.js
@@ -14,33 +14,33 @@ describe('Reflection', () => {
 	const fn = () => {};
 
 	/* Tests */
-	test('constructorName should return the constructor name '
+	test('constructorName returns the constructor name '
 		+ 'of the given value', () => {
 		expect(constructorName(obj)).toEqual('Object');
 		expect(constructorName(arr)).toEqual('Array');
 	});
 
-	test('inferType should infer the type of the given value', () => {
+	test('inferType infers the type of the given value', () => {
 		expect(inferType(obj)).toEqual('object');
 		expect(inferType(1)).toEqual('number');
 		expect(inferType(null)).toEqual('null');
 		expect(inferType(undefined)).toEqual('undefined');
 	});
 
-	test('isIterable should return true only when the given value '
+	test('isIterable returns true only when the given value '
 		+ 'is an Array or an Object', () => {
 		expect(isIterable(obj)).toEqual(true);
 		expect(isIterable(arr)).toEqual(true);
 		expect(isIterable(fn)).toEqual(false);
 	});
 
-	test('isFunction should return true only when the given value '
+	test('isFunction returns true only when the given value '
 		+ 'is a Function', () => {
 		expect(isFunction(obj)).toEqual(false);
 		expect(isFunction(fn)).toEqual(true);
 	});
 
-	test('isObject should return true only when the given value '
+	test('isObject returns true only when the given value '
 		+ 'is an Object', () => {
 		expect(isObject(obj)).toEqual(true);
 		expect(isObject(arr)).toEqual(false);
