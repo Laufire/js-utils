54568089b31acde57eacaf6c727f35ada406296b
laufire <laufiretechnologies@gmail.com>
2021-05-27 09:44:10 +0530

    Introduced sorters.configured.

--- src/sorters.js
+++ src/sorters.js
@@ -3,6 +3,10 @@
  *
  */
 
+/* Helpers */
+import { keys } from './lib';
+
+/* Exports */
 const ascending = (a, b) => (a > b ? 1 : a < b ? -1 : 0);
 
 const descending = (a, b) => (a < b ? 1 : a > b ? -1 : 0);
@@ -13,10 +17,35 @@ const reverse = () => -1;
 
 const onProp = (prop, sorter) => (a, b) => sorter(a[prop], b[prop]);
 
+const defaultGrammar = {
+	ascending,
+	descending,
+	existing,
+	reverse,
+};
+
+const configured = (config, grammarExt) => {
+	const grammar = { ...defaultGrammar, ...grammarExt };
+	const props = keys(config);
+
+	return (a, b) => {
+		for(let i = 0; i < props.length; ++i) {
+			const prop = props[i];
+			const result = grammar[config[prop]](a[prop], b[prop]);
+
+			if(result !== 0)
+				return result;
+		}
+
+		return 0;
+	};
+};
+
 export {
 	ascending,
 	descending,
 	existing,
 	reverse,
 	onProp,
+	configured,
 };
