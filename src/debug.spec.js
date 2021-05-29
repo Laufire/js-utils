5461a51b1d3c231557ec2385c5f13aa130af11c6
laufire <laufiretechnologies@gmail.com>
2021-05-29 16:27:02 +0530

    Introduced debug.peek.label.

--- src/debug.spec.js
+++ src/debug.spec.js
@@ -4,15 +4,26 @@ beforeEach(() => {
 	jest.clearAllMocks();
 });
 
-test('peek logs a value to the console and returns the same value.', () => {
+describe('peek - a drop in console.log replacement with better devEx', () => {
 	console.log = jest.fn(); // eslint-disable-line no-console
 
-	const val = 1;
+	test('peek logs a value to the console and returns the same value.', () => {
+		const val = 1;
 
-	const ret = peek(1);
+		const ret = peek(val);
 
-	expect(ret).toBe(val);
-	expect(console.log).toHaveBeenCalledWith(val); // eslint-disable-line no-console
+		expect(ret).toBe(val);
+		expect(console.log).toHaveBeenCalledWith(val); // eslint-disable-line no-console
+	});
+
+	test('peek supports an optional label.', () => {
+		const val = 1;
+		const label = 'someLabel';
+
+		const ret = peek(val, label);
+
+		expect(console.log).toHaveBeenCalledWith(label, val); // eslint-disable-line no-console
+	});
 });
 
 test('sleep stalls the flow for 1000ms by default.', async () => {
