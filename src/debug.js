28a0a9d9fc3ed51fc66d0a7d310748eaa944b008
Laufire <laufiretechnologies@gmail.com>
2021-05-29 16:27:02 +0530

    Introduced debug.peek.label.

--- src/debug.js
+++ src/debug.js
@@ -2,8 +2,10 @@
  * Utility functions to help with debugging.
  */
 
-const peek = (x) =>
-	(console.log(x), x); // eslint-disable-line no-console, no-sequences
+import { isDefined } from './reflection';
+
+const peek = (value, label) =>
+	(console.log(...isDefined(label) ? [label] : [], value), value); // eslint-disable-line no-console, no-sequences
 
 const sleep = (ms = 1000) => // eslint-disable-line no-magic-numbers
 	new Promise((resolve) => setTimeout(resolve, ms));
