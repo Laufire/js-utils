38455717e6e87f631abd481fa3a001cbe29d9b92
nkg-laufire <nkg@laufire.in>
2021-11-19 12:26:14 +0000

    Introduced a module path.

---
+++ src/path.spec.js
@@ -0,0 +1,86 @@
+import { keys, map, range } from './collection';
+import { fix, parts } from './path';
+import { rndString, rndValue, rndBetween } from './random';
+import { retry } from '../test/helpers';
+
+const getRndRange = () => {
+	const lowerLimit = 0;
+	const higherLimit = 8;
+
+	return range(0, rndBetween(lowerLimit, higherLimit));
+};
+
+const relativeDots = () => '.'.repeat(rndBetween(1, 5));
+
+const rndName = () => rndString(rndBetween(5, 10));
+
+const emptyName = () => '';
+
+const randomParts = () => map(getRndRange(), () =>
+	rndValue([emptyName, rndName, relativeDots])());
+
+const emptyParts = ['.', ''];
+
+const partPrefixes = {
+	relative: (pathParts) => [relativeDots(), ...pathParts],
+
+	lax: (pathParts) => [
+		...rndValue([[rndString()], emptyParts, [relativeDots()]]),
+		...pathParts,
+	],
+
+	absolute: (pathParts) => pathParts,
+};
+
+const isName = (initialPart) => (/^[^\\.]*$/).test(initialPart);
+
+const prefixes = {
+	absolute: (pathParts) => (pathParts.length ? '/' : ''),
+
+	relative: () => '',
+
+	lax: ([initialPart]) => (isName(initialPart) ? './' : ''),
+};
+
+const toLax = (fixed, pathParts) =>
+	fixed.slice((/[^\\.]/).test(pathParts[0]) ? 2 : 0)
+		.slice(0, pathParts[pathParts.length - 1] === '' ? undefined : -1);
+
+const generateCase = (type) => {
+	const pathParts = partPrefixes[type](randomParts());
+	const fixed = `${ prefixes[type](pathParts) }${ pathParts.join('/') }/`;
+	const fixedParts = [
+		...type === 'lax' && isName(pathParts[0]) ? ['.'] : [],
+		...pathParts,
+	];
+
+	const path = type !== 'lax'	? fixed	: toLax(fixed, pathParts);
+
+	return {
+		parts: fixedParts,
+		path: path,
+		fixed: fixed,
+		type: type,
+	};
+};
+
+describe('path', () => {
+	const combinations = retry(() =>
+		generateCase(rndValue(keys(partPrefixes))));
+
+	test('builds parts based on path', () => {
+		map(combinations, ({ path, parts: expected }) => {
+			const result = parts(path);
+
+			expect(result).toEqual(expected);
+		});
+	});
+
+	test('fixes the given path', () => {
+		map(combinations, ({ path, fixed: expected }) => {
+			const result = fix(path);
+
+			expect(result).toEqual(expected);
+		});
+	});
+});
