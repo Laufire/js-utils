9f076458b45f56a98b28c166e7791e7036c188a2
nkg-laufire <nkg@laufire.in>
2022-01-24 11:09:00 +0530

    Improved prob.possibilities to work with objects.

--- src/prob.js
+++ src/prob.js
@@ -1,34 +1,42 @@
 
-import { clone } from '@laufire/utils/collection';
+import { clone, fromEntries, map } from '@laufire/utils/collection';
 import { entries } from '@laufire/utils/lib';
+import { inferType } from '@laufire/utils/reflection';
 import { rndBetween } from './random';
 
 const isProbable = (probability) =>
 // eslint-disable-next-line no-magic-numbers
 	rndBetween(1, 101) <= probability * 100;
 
-// Const sample = { item: ['apple', 'banana'], price: [1, 2, 3] };
-// Const first = [{ item: 'apple' }, { item: 'banana' }];
-// Const result = [{ item: 'apple', price: 1 }, { item: 'apple', price: 2 }, { item: 'apple', price: 3 }];
-// Const sample = [['a', 'b', 'c'], [1, 2, 3]];
-// Const first = [['a'], ['b'], ['c']];
-
+// eslint-disable-next-line max-lines-per-function
 const possibilities = (data) => {
-	const [first, ...rest] = clone(data).reverse();
+	const arrayWorker = (source) => {
+		const [first, ...rest] = clone(source).reverse();
 
-	return rest.reduce((acc, currentArray) =>
-		currentArray.reduce((accOne, currentItem) =>
-			[...accOne, ...acc.map((prevItems) =>
-				[currentItem, ...prevItems])], [])
-	, first.map((x) => [x]));
+		return rest.reduce((acc, currentArray) =>
+			currentArray.reduce((accOne, currentItem) =>
+				[...accOne, ...acc.map((prevItems) =>
+					[currentItem, ...prevItems])], [])
+		, first.map((x) => [x]));
+	};
+
+	const objectWorker = (source) => {
+		const input = map(entries(source), (value) => {
+			const [name, combos] = value;
+
+			return arrayWorker([[name], combos]);
+		});
+		const result = arrayWorker(input);
+
+		return map(result, fromEntries);
+	};
+
+	const workers = {
+		object: objectWorker,
+		array: arrayWorker,
+	};
+
+	return workers[inferType(data)](data);
 };
 
-const ObjectPossibilities = () => {
-	const sample = { item: ['apple', 'banana'], price: [1, 2, 3] };
-	const result = entries(sample);
-	const combos = possibilities(result);
-
-	console.log;
-};
-
-export { isProbable, possibilities, ObjectPossibilities };
+export { isProbable, possibilities };
