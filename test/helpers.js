bae563b8a2fed1fa088955973f5d4d8d7cdc887f
mrc-laufire <mrc@laufire.in>
2022-01-27 15:38:06 +0530

    Introduced randomValues and arrayOrObject in testHelpers.

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,7 @@
 import {
 	clone, secure, map, reduce, shuffle,
 	keys, filter, range, dict, fromEntries, shell, has, values,
-	omit,
+	omit, clean, select,
 } from '@laufire/utils/collection';
 import { rndValue, rndBetween, rndString, rndValues }
 	from '@laufire/utils/random';
@@ -239,6 +239,16 @@ const getRatios = (iterable) => {
 	));
 };
 
+// TODO: Remove post publishing.
+const randomValues = (iterable) => {
+	const selector = rndKeys(iterable);
+
+	return secure(clean(select(iterable, selector)));
+};
+
+const arrayOrObject = (iterable) =>
+	secure(rndValue([values, toObject])(iterable));
+
 export {
 	contracted, array, object, cloned,
 	extension, extended, isolated, ecKeys,
@@ -248,5 +258,5 @@ export {
 	sortArray, strSubSet, retry, isAcceptable, expectEquals,
 	allTypes, emptyTypes, rnd, similarCols, iterableTypes,
 	till, findLastIndex, summarize, testRatios, getRatios,
-	convertKey,
+	convertKey, randomValues, arrayOrObject,
 };
