e3b4ddbb13530d3c45b1f6608760c0259464aafc
mrc-laufire <mrc@laufire.in>
2022-01-27 16:48:24 +0530

    Improved predicates spec.

--- src/predicates.spec.js
+++ src/predicates.spec.js
@@ -1,14 +1,12 @@
 /* Helpers */
-import { contains, filter, shares, map, values, has, dict } from
-	'@laufire/utils/collection';
+import { contains, filter, shares, values,
+	find, clone, shell,
+	clean, select, keys, equals,
+	secure, shuffle, omit } from '@laufire/utils/collection';
 import { rndValue, rndValues } from '@laufire/utils/random';
-import {
-	array, object, cloned, extension, extended, isolated,
-	collection, extCollection, sortArray,
-	contracted, rndKey, ecKeys,
-} from '../test/helpers';
-import { secure, shuffle } from './collection';
-import { keys, rndBetween } from './lib';
+import { isolated, collection, extCollection, sortArray,
+	rndKey, rndCollection, retry, rndDict,
+	randomValues, arrayOrObject, rndNested } from '../test/helpers';
 
 /* Tested */
 import { isEqual, isSame, isPart, doesContain,
@@ -17,64 +15,181 @@ import { isEqual, isSame, isPart, doesContain,
 	not, or, and, onProp,
 	predicate, isIn, value, key, is } from './predicates';
 
+/* Helpers */
+
+// eslint-disable-next-line no-shadow
+const testIterator = ({ predicate, iterable, expectation }) =>
+	expect(clean(filter(iterable, predicate))).toEqual(expectation);
+
+// TODO: Remove post fixing the testHelpers.rndCollection.
+const randomCollection = () => rndValue([values, (x) => x])(rndDict());
+
 /* Spec */
 describe('Predicates', () => {
 	const truthies = [1, '2', true, [], {}];
 	const falsies = [0, '', false, undefined, null];
 	const tAndFArray = secure(shuffle(truthies.concat(falsies)));
+	const tasks = [
+		{ name: 'commit', effort: 1 },
+		{ name: 'commit', effort: 2 },
+		{ name: 'push', effort: 3 },
+	];
 
-	test('isEqual returns a function to test value equality'
-		+ ' between the candidates.', () => {
-		expect(filter(collection, isEqual(object))).toEqual(collection);
+	describe('isEqual returns a predicate to test value equality'
+	+ ' between the candidates.', () => {
+		test('example', () => {
+			const taskToFind = { name: 'commit', effort: 1 };
+
+			expect(find(tasks, isEqual(taskToFind))).toEqual(taskToFind);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndCollection();
+				const needle = clone(rndValue(haystack));
+
+				expect(find(haystack, isEqual(needle))).toEqual(needle);
+			});
+		});
 	});
 
-	test('isSame returns a function to test referential equality'
-		+ ' between the candidates', () => {
-		expect(filter(collection, isSame(collection.object)).object)
-			.toBe(collection.object);
+	describe('isSame returns a predicate to test referential equality'
+	+ ' between the candidates', () => {
+		test('example', () => {
+			const taskToFind = tasks[0];
+
+			expect(find(tasks, isSame(taskToFind))).toEqual(taskToFind);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndCollection();
+				const needle = rndValue(haystack);
+
+				expect(find(haystack, isSame(needle))).toEqual(needle);
+			});
+		});
 	});
 
-	test('is, an alias for isSame', () => {
+	test('is is an alias for isSame', () => {
 		expect(is).toBe(isSame);
 	});
 
-	test('isPart returns a function to test whether the tested object is'
-		+ ' wholly contained in any of the elements', () => {
-		expect(filter(collection, isPart(extended))).toEqual(collection);
-		expect(filter(collection, isPart(isolated))).toEqual({});
+	describe('isPart returns a predicate to test whether the tested object is'
+	+ ' wholly contained in any of the elements', () => {
+		test('example', () => {
+			const taskToFind = { ...tasks[0], author: 'author' };
+
+			expect(find(tasks, isPart(taskToFind))).toEqual(tasks[0]);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndNested(
+					3, 3, ['nested']
+				);
+				const needle = rndValue(haystack);
+				const extendedNeedle = { ...needle, ...rndDict() };
+				const partialNeedle = randomValues(needle);
+
+				expect(find(haystack, isPart(extendedNeedle))).toEqual(needle);
+				expect(find(haystack, isPart(partialNeedle)))
+					.toEqual(undefined);
+			});
+		});
 	});
 
-	test('doesContain returns a function to test whether the tested object is'
-		+ 'partially contained in any of the elements', () => {
-		expect(filter(collection, doesContain(contracted)))
-			.toEqual(collection);
-		expect(filter(collection, doesContain(isolated)))
-			.toEqual({});
+	describe('doesContain returns a predicate to test whether the'
+	+ ' tested object is partially contained in any of the elements', () => {
+		test('example', () => {
+			const needle = { name: 'push' };
+			const expectation = { name: 'push', effort: 3 };
+
+			expect(find(tasks, doesContain(needle))).toEqual(expectation);
+			expect(find(tasks, doesContain({ name: Symbol('name') })))
+				.toEqual(undefined);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndNested(
+					3, 3, ['object']
+				);
+				const expectation = rndValue(haystack);
+				const needle = randomValues(expectation);
+
+				expect(find(haystack, doesContain(needle)))
+					.toEqual(expectation);
+			});
+		});
 	});
 
-	test('truthy tests for truthy values', () => {
-		expect(sortArray(tAndFArray.filter(truthy)))
-			.toEqual(sortArray(truthies));
-		expect(sortArray(falsies.filter(truthy))).toEqual([]);
+	describe('truthy tests for truthy values', () => {
+		test('example', () => {
+			// TODO: Replace with collections.filter post publishing.
+			expect([0, 1, 'a', null].filter(truthy)).toEqual([1, 'a']);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const tAndFIterable = arrayOrObject(tAndFArray);
+				const fIterable = arrayOrObject(falsies);
+
+				expect(sortArray(values(filter(tAndFIterable, truthy))))
+					.toEqual(sortArray(truthies));
+				expect(filter(fIterable, truthy)).toEqual(shell(fIterable));
+			});
+		});
 	});
 
-	test('falsy tests for falsy values', () => {
-		expect(sortArray(tAndFArray.filter(falsy)))
-			.toEqual(sortArray(falsies));
-		expect(sortArray(truthies.filter(falsy))).toEqual([]);
+	describe('falsy tests for falsy values', () => {
+		test('example', () => {
+			// TODO: Replace with collections.filter post publishing.
+			expect([0, 1, 'a', null].filter(falsy)).toEqual([0, null]);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const tAndFIterable = arrayOrObject(tAndFArray);
+				const tIterable = arrayOrObject(truthies);
+
+				expect(sortArray(values(filter(tAndFIterable, falsy))))
+					.toEqual(sortArray(falsies));
+				expect(filter(tIterable, falsy)).toEqual(shell(tIterable));
+			});
+		});
 	});
 
-	test('everything allows everything through the filter.', () => {
-		expect(sortArray(tAndFArray.filter(everything)))
-			.toEqual(sortArray(tAndFArray));
+	describe('everything allows everything through the filter', () => {
+		test('example', () => {
+			expect(filter(tasks, everything)).toEqual(tasks);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const iterable = rndCollection();
+
+				expect(filter(iterable, everything)).toEqual(iterable);
+			});
+		});
 	});
 
-	test('nothing allows nothing through the filter.', () => {
-		expect(sortArray(tAndFArray.filter(nothing))).toEqual([]);
+	describe('nothing allows nothing through the filter', () => {
+		test('example', () => {
+			expect(filter(tasks, nothing)).toEqual([]);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const iterable = rndCollection();
+
+				expect(filter(iterable, nothing)).toEqual(shell(iterable));
+			});
+		});
 	});
 
-	test('first tests for the first occurrence of the element in '
-	+ 'the collection.', () => {
+	test('first tests for the first occurrence of the element in'
+	+ ' the collection', () => {
 		expect(truthies.concat(truthies).filter(first)).toEqual(truthies);
 	});
 
@@ -82,56 +197,117 @@ describe('Predicates', () => {
 		expect(unique).toBe(first);
 	});
 
-	test('not returns the inverse of the given predicate.', () => {
-		expect(filter(collection, not(isEqual(object)))).not
-			.toEqual(collection);
-		expect(filter(collection, not(isSame(object))).object).not
-			.toBe(object);
-		expect(filter(collection, not(isPart(extended))).object).not
-			.toBe(object);
+	describe('not returns the inverse of the given predicate', () => {
+		test('example', () => {
+			// eslint-disable-next-line no-shadow
+			const predicate = not(isEqual({ name: 'commit', effort: 1 }));
+			const expectation = [
+				{ name: 'commit', effort: 2 },
+				{ name: 'push', effort: 3 },
+			];
 
-		const expectations = [
-			[truthy, falsies],
-			[falsy, truthies],
-			[nothing, tAndFArray],
-			[everything, []],
-		];
+			// TODO: Remove clean post publishing.
+			expect(clean(filter(tasks, predicate))).toEqual(expectation);
+		});
 
-		map(expectations, ([fn, expected]) => {
-			expect(sortArray(tAndFArray.filter(not(fn))))
-				.toEqual(sortArray(expected));
+		test('randomized test', () => {
+			retry(() => {
+				const iterable = randomCollection();
+				const selector = rndKey(iterable);
+				const needle = iterable[selector];
+				// eslint-disable-next-line no-shadow
+				const predicate = not(isEqual(needle));
+				// TODO: Remove clean post publishing.
+				const expectation = clean(omit(iterable, [selector]));
+
+				testIterator({ predicate, iterable, expectation });
+			});
 		});
 	});
 
-	test('and returns a function to test the candidates to pass'
-	+ ' all the given predicates.', () => {
-		expect(filter(collection, and(isSame(object), isSame(cloned))))
-			.toEqual({});
-		expect(filter(collection, and(isSame(object), isEqual(cloned))))
-			.toEqual({ [ecKeys.object]: object });
+	describe('and returns a predicate to test the candidates to pass'
+	+ ' all the given predicates', () => {
+		test('example', () => {
+			const taskToFind = rndValue(tasks);
+			// eslint-disable-next-line no-shadow
+			const predicate = and(isSame(taskToFind), isEqual(taskToFind));
+			const partialTasks = filter(tasks, not(isEqual(taskToFind)));
+
+			expect(find(tasks, predicate)).toEqual(taskToFind);
+			expect(find(partialTasks, predicate)).toEqual(undefined);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const iterable = randomCollection();
+				const selector = rndKey(iterable);
+				const needle = iterable[selector];
+				// eslint-disable-next-line no-shadow
+				const predicate = and(isEqual(needle), isSame(needle));
+				const expectation = clean(select(iterable, [selector]));
+
+				testIterator({ predicate, iterable, expectation });
+			});
+		});
 	});
 
-	test('or returns a function to test the candidates to pass'
-	+ ' at least one among multiple predicates.', () => {
-		expect(filter(collection, or(isSame(object), isSame(cloned))))
-			.toEqual(collection);
-		expect(filter(collection, or(isSame(extended), isEqual(extended))))
-			.toEqual({});
+	describe('or returns a predicate to test the candidates to pass'
+	+ ' at least one among multiple predicates', () => {
+		test('example', () => {
+			const taskToFind = tasks[1];
+			// eslint-disable-next-line no-shadow
+			const predicate = or(isSame(taskToFind),
+				isEqual(Symbol('additionalTask')));
+			const partialTasks = filter(tasks, not(isEqual(taskToFind)));
+
+			expect(find(tasks, predicate)).toEqual(taskToFind);
+			expect(find(partialTasks, predicate)).toEqual(undefined);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const iterable = randomCollection();
+				const selector = rndValues(keys(iterable), 2);
+				const needleOne = iterable[selector[0]];
+				const needleTwo = iterable[selector[1]];
+				// eslint-disable-next-line no-shadow
+				const predicate = or(isSame(needleOne), isSame(needleTwo));
+				const expectation = clean(select(iterable, selector));
+
+				testIterator({ predicate, iterable, expectation });
+			});
+		});
 	});
 
-	test('onProp returns a function to test the given'
-	+ 'prop across candidates'
-	+ ' of a collection.', () => {
-		const prop = rndKey(isolated);
+	describe('onProp returns a predicate to test the given prop across'
+	+ ' candidates of a collection', () => {
+		test('example', () => {
+			const prop = 'name';
+			const taskToFind = 'push';
+			// eslint-disable-next-line no-shadow
+			const predicate = onProp(prop, isEqual(taskToFind));
+			const expectation = { name: 'push', effort: 3 };
 
-		expect(filter({ ...extCollection, isolated },
-			onProp(prop, isEqual(isolated[prop]))))
-			// TODO: Randomize the key.
-			.toEqual({ isolated });
+			expect(find(tasks, predicate)).toEqual(expectation);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndNested(
+					3, 3, ['nested']
+				);
+				const expectation = rndValue(haystack);
+				const prop = rndKey(expectation);
+				const needle = expectation[prop];
+
+				expect(find(haystack, onProp(prop, isEqual(needle))))
+					.toEqual(expectation);
+			});
+		});
 	});
 
 	describe('generators pass all available arguments'
-	+ 'to the given predicates.', () => {
+	+ ' to the given predicates', () => {
 		const childCollection = rndValue(collection);
 		const childKey = rndKey(childCollection);
 		const generators = {
@@ -163,54 +339,101 @@ describe('Predicates', () => {
 		});
 	});
 
-	test('predicate derives predicates from relevant'
+	describe('predicate derives predicates from relevant'
 	+ ' collection functions', () => {
-		const childKey = rndKey(extCollection);
-		const childCollection = extCollection[childKey];
-		const mockCollection = { ...extCollection, isolated };
+		test('example', () => {
+			const taskToFind = tasks[1];
 
-		expect(filter(mockCollection, predicate(isSame(childCollection))))
-			.toEqual({ [childKey]: childCollection });
+			expect(find(tasks, predicate(equals, taskToFind)))
+				.toEqual(taskToFind);
+			expect(find(tasks, predicate(contains, taskToFind)))
+				.toEqual(taskToFind);
+		});
 
-		expect(filter(mockCollection, predicate(contains, isolated)))
-			.toEqual({ isolated });
+		test('detailed example', () => {
+			const mockCollection = { ...extCollection, isolated };
 
-		expect(filter(mockCollection, predicate(
-			shares, isolated, rndKey(isolated)
-		)))
-			.toEqual({ isolated });
+			expect(filter(mockCollection, predicate(
+				shares, isolated, rndKey(isolated)
+			))).toEqual({ isolated });
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndNested(
+					3, 3, ['nested', 'object', 'array']
+				);
+				const needle = rndValue(haystack);
+				const predicateFn = predicate(
+					shares, needle, rndKey(needle)
+				);
+
+				expect(find(haystack, predicateFn)).toEqual(needle);
+			});
+		});
 	});
 
-	test('isIn returns a predicate to check for a given values'
+	describe('isIn returns a predicate to check for a given values'
 	+ ' in collections', () => {
-		map([array, object], (iterable) => {
-			const haystackObject = dict(rndValues(values(iterable),
-				rndBetween(0, keys(iterable).length - 1)));
-			const haystackArray = values(haystackObject);
-			const predicateFn = (left) => (right) => has(left, right);
+		test('example', () => {
+			const iterable = [...tasks, { name: 'rebase', effort: 3 }];
 
-			map([haystackArray, haystackObject], (haystack) => {
-				const expectation = filter(iterable, predicateFn(haystack));
+			expect(filter(iterable, isIn(tasks))).toEqual(tasks);
+		});
 
-				expect(filter(iterable, isIn(haystack)))
+		test('randomized test', () => {
+			const iterable = rndCollection();
+			const needle = randomValues(iterable);
+
+			// TODO: Remove clean post publishing.
+			expect(clean(filter(iterable, isIn(needle)))).toEqual(needle);
+		});
+	});
+
+	describe('key passes the keys of iterated iterables'
+	+ ' to the given predicate', () => {
+		test('example', () => {
+			const cart = {
+				item: 'apple',
+				price: 100,
+			};
+
+			expect(find(cart, key(isEqual('item')))).toEqual('apple');
+			expect(find(cart, key(isEqual('price')))).toEqual(100);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndCollection();
+				// TODO: Remove String function post publishing.
+				const needle = String(rndKey(haystack));
+				const expectation = haystack[needle];
+
+				expect(find(haystack, key(isEqual(needle))))
 					.toEqual(expectation);
 			});
 		});
 	});
 
-	test('key passes the keys of iterated iterables'
-	+ ' to the given predicate.', () => {
-		const collectionKey = rndKey(collection);
-
-		expect(filter(collection, key(isEqual(collectionKey))))
-			.toEqual({ [collectionKey]: collection[collectionKey] });
-	});
-
-	test('value passes the values of iterated iterables'
+	describe('value passes the values of iterated iterables'
 	+ ' to the given predicate', () => {
-		const extensionKey = rndKey(extension);
+		test('example', () => {
+			const cart = {
+				item: 'apple',
+				price: 100,
+			};
 
-		expect(filter(extended, value(isEqual(extension[extensionKey]))))
-			.toEqual({ [extensionKey]: extended[extensionKey] });
+			expect(find(cart, value(isEqual('apple')))).toEqual('apple');
+			expect(find(cart, value(isEqual(100)))).toEqual(100);
+		});
+
+		test('randomized test', () => {
+			retry(() => {
+				const haystack = rndCollection();
+				const needle = rndValue(haystack);
+
+				expect(find(haystack, value(isEqual(needle)))).toEqual(needle);
+			});
+		});
 	});
 });
